f36dbc83258bca7ea2b18bd19f755719e550011f
Pham Quang Huy <pqhuy87it@gmail.com>
2020-09-19 22:31:28 +0900

    Add webview.

---
+++ WebView/WebKitDemo - Custom wkwebiew hien thi ca progress/WebKitDemo/WebViewController.swift
@@ -0,0 +1,251 @@
+//
+//  WebViewController.swift
+//  WebKitDemo
+//
+//  Created by Kyusaku Mihara on 9/17/14.
+//  Copyright (c) 2014 epohsoft. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+var myContext = 0
+
+class WebViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
+
+    weak var webView: WKWebView?
+    weak var progressView: WebViewProgressView?
+    @IBOutlet weak var backBarButton: UIBarButtonItem!
+    @IBOutlet weak var forwardBarButton: UIBarButtonItem!
+    @IBOutlet weak var stopBarButton: UIBarButtonItem!
+    @IBOutlet weak var refreshBarButton: UIBarButtonItem!
+
+    deinit {
+        webView?.removeObserver(self, forKeyPath: "loading")
+        webView?.removeObserver(self, forKeyPath: "title")
+        webView?.removeObserver(self, forKeyPath: "estimatedProgress")
+        webView?.removeObserver(self, forKeyPath: "canGoBack")
+        webView?.removeObserver(self, forKeyPath: "canGoForward")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let webViewConfiguration = WKWebViewConfiguration()
+
+        let webView = WKWebView(frame: view.bounds, configuration: webViewConfiguration)
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
+        webView.allowsBackForwardNavigationGestures = true
+        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        webView.addObserver(self, forKeyPath: "loading", options: .new, context: &myContext)
+        webView.addObserver(self, forKeyPath: "title", options: .new, context: &myContext)
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: &myContext)
+        webView.addObserver(self, forKeyPath: "canGoBack", options: .new, context: &myContext)
+        webView.addObserver(self, forKeyPath: "canGoForward", options: .new, context: &myContext)
+        view.addSubview(webView)
+        self.webView = webView
+
+        let navigationBarBounds = navigationController?.navigationBar.bounds
+        let progressView = WebViewProgressView(frame: CGRect(x: 0, y: navigationBarBounds!.size.height - 2, width: navigationBarBounds!.size.width, height: 2))
+        progressView.autoresizingMask = [.flexibleWidth, .flexibleTopMargin]
+        navigationController?.navigationBar.addSubview(progressView)
+        self.progressView = progressView
+
+        webView.load(URLRequest(url: URL(string: "https://www.google.co.jp")!))
+
+        backBarButton.isEnabled = false
+        forwardBarButton.isEnabled = false
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        guard let change = change else { return }
+        if context != &myContext {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+            return
+        }
+
+        if keyPath == "loading" {
+            if let loading = (change[NSKeyValueChangeKey.newKey] as AnyObject).boolValue {
+                stopBarButton?.isEnabled = loading
+                refreshBarButton?.isEnabled = !loading
+            }
+            return
+        }
+
+        if keyPath == "title" {
+            if let title = change[NSKeyValueChangeKey.newKey] as? String {
+                navigationItem.title = title
+            }
+            return
+        }
+
+        if keyPath == "estimatedProgress" {
+            if let progress = (change[NSKeyValueChangeKey.newKey] as AnyObject).floatValue {
+                progressView?.setProgress(progress, animated: true)
+            }
+            return
+        }
+        
+        if keyPath == "canGoBack" {
+            if let canGoBack = (change[NSKeyValueChangeKey.newKey] as AnyObject).boolValue {
+                backBarButton.isEnabled = canGoBack
+            }
+            return
+        }
+        
+        if keyPath == "canGoForward" {
+            if let canGoForward = (change[NSKeyValueChangeKey.newKey] as AnyObject).boolValue {
+                forwardBarButton.isEnabled = canGoForward
+            }
+            return
+        }
+    }
+
+    @IBAction func backBarButtonTapped(_ sender: AnyObject) {
+        if webView!.canGoBack {
+            webView!.goBack()
+        }
+    }
+
+    @IBAction func forwardBarButtonTapped(_ sender: AnyObject) {
+        if webView!.canGoForward {
+            webView!.goForward()
+        }
+    }
+
+    @IBAction func stopBarButtonTapped(_ sender: AnyObject) {
+        if webView!.isLoading {
+            webView!.stopLoading()
+        }
+    }
+
+    @IBAction func refreshBarButtonTapped(_ sender: AnyObject) {
+        _ = webView?.reload()
+    }
+
+    // MARK: - WKNavigationDelegate methods
+
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation) {
+        print("webView:\(webView) didStartProvisionalNavigation:\(navigation)")
+    }
+
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation) {
+        print("webView:\(webView) didCommitNavigation:\(navigation)")
+    }
+
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: (@escaping (WKNavigationActionPolicy) -> Void)) {
+        print("webView:\(webView) decidePolicyForNavigationAction:\(navigationAction) decisionHandler:\(String(describing: decisionHandler))")
+
+        switch navigationAction.navigationType {
+        case .linkActivated:
+            if navigationAction.targetFrame == nil {
+                webView.load(navigationAction.request)
+            }
+        default:
+            break
+        }
+
+        decisionHandler(.allow)
+    }
+
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: (@escaping (WKNavigationResponsePolicy) -> Void)) {
+        print("webView:\(webView) decidePolicyForNavigationResponse:\(navigationResponse) decisionHandler:\(String(describing: decisionHandler))")
+
+        decisionHandler(.allow)
+    }
+
+    func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        print("webView:\(webView) didReceiveAuthenticationChallenge:\(challenge) completionHandler:\(String(describing: completionHandler))")
+
+        switch (challenge.protectionSpace.authenticationMethod) {
+        case NSURLAuthenticationMethodHTTPBasic:
+            let alertController = UIAlertController(title: "Authentication Required", message: webView.url?.host, preferredStyle: .alert)
+            weak var usernameTextField: UITextField!
+            alertController.addTextField { textField in
+                textField.placeholder = "Username"
+                usernameTextField = textField
+            }
+            weak var passwordTextField: UITextField!
+            alertController.addTextField { textField in
+                textField.placeholder = "Password"
+                textField.isSecureTextEntry = true
+                passwordTextField = textField
+            }
+            alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { action in
+                completionHandler(.cancelAuthenticationChallenge, nil)
+            }))
+            alertController.addAction(UIAlertAction(title: "Log In", style: .default, handler: { action in
+                guard let username = usernameTextField.text, let password = passwordTextField.text else {
+                    completionHandler(.rejectProtectionSpace, nil)
+                    return
+                }
+                let credential = URLCredential(user: username, password: password, persistence: URLCredential.Persistence.forSession)
+                completionHandler(.useCredential, credential)
+            }))
+            present(alertController, animated: true, completion: nil)
+        default:
+            completionHandler(.rejectProtectionSpace, nil);
+        }
+    }
+
+    func webView(_ webView: WKWebView, didReceiveServerRedirectForProvisionalNavigation navigation: WKNavigation) {
+        print("webView:\(webView) didReceiveServerRedirectForProvisionalNavigation:\(navigation)")
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation) {
+        print("webView:\(webView) didFinishNavigation:\(navigation)")
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation, withError error: Error) {
+        print("webView:\(webView) didFailNavigation:\(navigation) withError:\(error)")
+    }
+
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation, withError error: Error) {
+        print("webView:\(webView) didFailProvisionalNavigation:\(navigation) withError:\(error)")
+    }
+
+    // MARK: WKUIDelegate methods
+    
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: (@escaping () -> Void)) {
+        print("webView:\(webView) runJavaScriptAlertPanelWithMessage:\(message) initiatedByFrame:\(frame) completionHandler:\(String(describing: completionHandler))")
+        
+        let alertController = UIAlertController(title: frame.request.url?.host, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            completionHandler()
+        }))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: (@escaping (Bool) -> Void)) {
+        print("webView:\(webView) runJavaScriptConfirmPanelWithMessage:\(message) initiatedByFrame:\(frame) completionHandler:\(String(describing: completionHandler))")
+        
+        let alertController = UIAlertController(title: frame.request.url?.host, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { action in
+            completionHandler(false)
+        }))
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            completionHandler(true)
+        }))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
+        print("webView:\(webView) runJavaScriptTextInputPanelWithPrompt:\(prompt) defaultText:\(String(describing: defaultText)) initiatedByFrame:\(frame) completionHandler:\(String(describing: completionHandler))")
+        
+        let alertController = UIAlertController(title: frame.request.url?.host, message: prompt, preferredStyle: .alert)
+        weak var alertTextField: UITextField!
+        alertController.addTextField { textField in
+            textField.text = defaultText
+            alertTextField = textField
+        }
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { action in
+            completionHandler(nil)
+        }))
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            completionHandler(alertTextField.text)
+        }))
+        present(alertController, animated: true, completion: nil)
+    }
+
+}
---
+++ WebView/WebKitDemo - Custom wkwebiew hien thi ca progress/WebKitDemo/WebViewProgressView.swift
@@ -0,0 +1,92 @@
+//
+//  WebViewProgressView.swift
+//  WebKitDemo
+//
+//  porting NJKWebViewProgress to Swift.
+//  see also https://github.com/ninjinkun/NJKWebViewProgress
+//
+//  Created by Kyusaku Mihara on 9/17/14.
+//  Copyright (c) 2014 epohsoft. All rights reserved.
+//
+
+import UIKit
+
+class WebViewProgressView: UIView {
+
+    var barAnimationDuration = 0.27
+    var fadeAnimationDuration = 0.27
+    var fadeOutDelay = 0.1
+    var progress: Float {
+        get {
+            return _progress
+        }
+        set {
+            setProgress(newValue, animated: false)
+        }
+    }
+
+    private var progressView = UIView()
+    private var _progress: Float = 0.0
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureViews()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configureViews()
+    }
+
+    func setProgress(_ progress: Float, animated: Bool) {
+        _progress = progress
+        let isGrowing = progress > 0.0
+        UIView.animate(
+            withDuration: (isGrowing && animated) ? barAnimationDuration: 0.0,
+            delay: 0,
+            options: UIView.AnimationOptions(),
+            animations: {
+                self.progressView.frame.size.width = CGFloat(progress) * self.bounds.size.width
+            },
+            completion: nil
+        )
+
+        if progress >= 1.0 {
+            UIView.animate(
+                withDuration: animated ? barAnimationDuration : 0.0,
+                delay: fadeOutDelay,
+                options: UIView.AnimationOptions(),
+                animations: {
+                    self.progressView.alpha = 0.0
+                }, completion: { completed in
+                    self.progressView.frame.size.width = 0.0
+                }
+            )
+        } else {
+            UIView.animate(
+                withDuration: animated ? barAnimationDuration : 0.0,
+                delay: fadeOutDelay,
+                options: UIView.AnimationOptions(),
+                animations: {
+                    self.progressView.alpha = 1.0
+                },
+                completion: nil
+            )
+        }
+    }
+
+    private func configureViews() {
+        isUserInteractionEnabled = false;
+        autoresizingMask = .flexibleWidth
+
+        progressView.frame = bounds
+        progressView.frame.size.width = 0
+        progressView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        progressView.backgroundColor = tintColor
+        addSubview(progressView)
+
+        barAnimationDuration = 0.27
+        fadeAnimationDuration = 0.27
+        fadeOutDelay = 0.1
+    }
+}
