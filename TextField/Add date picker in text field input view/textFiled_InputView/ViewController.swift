7449d8359739951be50b262c7d666a22d28de557
Pham Quang Huy <pqhuy87it@gmail.com>
2020-09-19 22:35:07 +0900

    Add text field.

---
+++ TextField/Add date picker in text field input view/textFiled_InputView/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  textFiled_InputView
+//
+//  Created by Nimble Chapps on 26/03/16.
+//  Copyright © 2016 Nimble Chapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController , UITextFieldDelegate{
+
+    @IBOutlet weak var textField_Date: UITextField!
+    var datePicker : UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK:- textFiled Delegate
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.pickUpDate(textField: self.textField_Date)
+    }
+    
+    //MARK:- Function of datePicker 
+    func pickUpDate(textField : UITextField){
+        
+        // DatePicker
+        self.datePicker = UIDatePicker(frame:CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 216))
+        self.datePicker.backgroundColor = UIColor.white
+        self.datePicker.datePickerMode = UIDatePicker.Mode.date
+        textField.inputView = self.datePicker
+        
+        // ToolBar
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor(red: 92/255, green: 216/255, blue: 255/255, alpha: 1)
+        toolBar.sizeToFit()
+        
+        // Adding Button ToolBar
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(ViewController.doneClick))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(ViewController.cancelClick))
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        textField.inputAccessoryView = toolBar
+        
+    }
+    
+    // MARK:- Button Done and Cancel
+    @objc func doneClick() {
+        let dateFormatter1 = DateFormatter()
+        dateFormatter1.dateStyle = .medium
+        dateFormatter1.timeStyle = .none
+        textField_Date.text = dateFormatter1.string(from: datePicker.date)
+        textField_Date.resignFirstResponder()
+    }
+    @objc func cancelClick() {
+        textField_Date.resignFirstResponder()
+    }
+
+}
+
